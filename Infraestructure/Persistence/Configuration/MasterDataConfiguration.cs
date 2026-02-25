5d5b9a5f594f86352596c18c1ac101c644309da9
luisf <luisf@myprocess.net>
2026-02-24 19:35:16 -0500

    namespaace

--- Infraestructure/Persistence/Configuration/MasterDataConfiguration.cs
+++ Infraestructure/Persistence/Configuration/MasterDataConfiguration.cs
@@ -1,24 +1,18 @@
 ﻿using Domain.Entities;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Persistence.Configuration
+namespace Persistence.Configuration;
+
+public class MasterDataConfiguration : IEntityTypeConfiguration<MasterDataEntity>
 {
-    public class MasterDataConfiguration : IEntityTypeConfiguration<MasterDataEntity>
+    public void Configure(EntityTypeBuilder<MasterDataEntity> b)
     {
-        public void Configure(EntityTypeBuilder<MasterDataEntity> b)
-        {
-            b.ToTable("MasterData");
-            b.HasKey(x => x.Id);
+        b.ToTable("MasterData");
+        b.HasKey(x => x.Id);
 
-            b.Property(x => x.Name).HasMaxLength(120).IsRequired();
-            b.HasIndex(x => x.Name).IsUnique();
+        b.Property(x => x.Name).HasMaxLength(120).IsRequired();
+        b.HasIndex(x => x.Name).IsUnique();
 
-        }
     }
 }
--- Infraestructure/Persistence/Configuration/UserConfiguration.cs
+++ Infraestructure/Persistence/Configuration/UserConfiguration.cs
@@ -2,19 +2,18 @@
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.EntityFrameworkCore;
 
-namespace Persistence.Configuration
+namespace Persistence.Configuration;
+
+public class UserConfiguration : IEntityTypeConfiguration<UserEntity>
 {
-    public class UserConfiguration : IEntityTypeConfiguration<UserEntity>
+    public void Configure(EntityTypeBuilder<UserEntity> b)
     {
-        public void Configure(EntityTypeBuilder<UserEntity> b)
-        {
-            b.ToTable("Users");
-            b.HasKey(x => x.Id);
+        b.ToTable("Users");
+        b.HasKey(x => x.Id);
 
-            b.Property(x => x.Name).HasMaxLength(120).IsRequired();
-            b.Property(x => x.Email).HasMaxLength(256).IsRequired();
+        b.Property(x => x.Name).HasMaxLength(120).IsRequired();
+        b.Property(x => x.Email).HasMaxLength(256).IsRequired();
 
-            b.HasIndex(x => x.Email).IsUnique();
-        }
+        b.HasIndex(x => x.Email).IsUnique();
     }
 }
